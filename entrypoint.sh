91d1dc482d23c9c1b69a6a034965020c65bb469d
carazzim0 <basti.carazzimo@gmail.com>
2016-10-09 13:27:08 +0200

    * modify sed-seperator to pipe * add node_startup_method from recent ebot update

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,19 +14,21 @@ DEMO_DOWNLOAD="${DEMO_DOWNLOAD:-true}"
 REMIND_RECORD="${REMIND_RECORD:-false}"
 DAMAGE_REPORT="${DAMAGE_REPORT:-false}"
 DELAY_READY="${DELAY_READY:-false}"
+NODE_STARTUP_METHOD="${NODE_STARTUP_METHOD:-forever start}"
 
-sed -i "s/BOT_IP =.*/BOT_IP = \"$CONTAINER_IP\"/g" $EBOT_HOME/config/config.ini
-sed -i "s/EXTERNAL_LOG_IP = .*/EXTERNAL_LOG_IP = \"$EXTERNAL_IP\"/g" $EBOT_HOME/config/config.ini
-sed -i "s/MYSQL_IP =.*/MYSQL_IP = \"$MYSQL_HOST\"/g" $EBOT_HOME/config/config.ini
-sed -i "s/MYSQL_PORT =.*/MYSQL_PORT = \"$MYSQL_PORT\"/g" $EBOT_HOME/config/config.ini
-sed -i "s/MYSQL_USER =.*/MYSQL_USER = \"$MYSQL_USER\"/g" $EBOT_HOME/config/config.ini
-sed -i "s/MYSQL_PASS =.*/MYSQL_PASS = \"$MYSQL_PASS\"/g" $EBOT_HOME/config/config.ini
-sed -i "s/MYSQL_BASE =.*/MYSQL_BASE = \"$MYSQL_DB\"/g" $EBOT_HOME/config/config.ini
-sed -i "s/LO3_METHOD =.*/LO3_METHOD = \"$LO3_METHOD\"/g" $EBOT_HOME/config/config.ini
-sed -i "s/KO3_METHOD =.*/KO3_METHOD = \"$KO3_METHOD\"/g" $EBOT_HOME/config/config.ini
-sed -i "s/DEMO_DOWNLOAD =.*/DEMO_DOWNLOAD = $DEMO_DOWNLOAD/g" $EBOT_HOME/config/config.ini
-sed -i "s/REMIND_RECORD =.*/REMIND_RECORD = $REMIND_RECORD/g" $EBOT_HOME/config/config.ini
-sed -i "s/DAMAGE_REPORT =.*/DAMAGE_REPORT = $DAMAGE_REPORT/g" $EBOT_HOME/config/config.ini
-sed -i "s/DELAY_READY = .*/DELAY_READY = $DELAY_READY/g" $EBOT_HOME/config/config.ini
+sed -i "s|BOT_IP =.*|BOT_IP = \"$CONTAINER_IP\"|g" $EBOT_HOME/config/config.ini
+sed -i "s|EXTERNAL_LOG_IP = .*|EXTERNAL_LOG_IP = \"$EXTERNAL_IP\"|g" $EBOT_HOME/config/config.ini
+sed -i "s|MYSQL_IP =.*|MYSQL_IP = \"$MYSQL_HOST\"|g" $EBOT_HOME/config/config.ini
+sed -i "s|MYSQL_PORT =.*|MYSQL_PORT = \"$MYSQL_PORT\"|g" $EBOT_HOME/config/config.ini
+sed -i "s|MYSQL_USER =.*|MYSQL_USER = \"$MYSQL_USER\"|g" $EBOT_HOME/config/config.ini
+sed -i "s|MYSQL_PASS =.*|MYSQL_PASS = \"$MYSQL_PASS\"|g" $EBOT_HOME/config/config.ini
+sed -i "s|MYSQL_BASE =.*|MYSQL_BASE = \"$MYSQL_DB\"|g" $EBOT_HOME/config/config.ini
+sed -i "s|LO3_METHOD =.*|LO3_METHOD = \"$LO3_METHOD\"|g" $EBOT_HOME/config/config.ini
+sed -i "s|KO3_METHOD =.*|KO3_METHOD = \"$KO3_METHOD\"|g" $EBOT_HOME/config/config.ini
+sed -i "s|DEMO_DOWNLOAD =.*|DEMO_DOWNLOAD = $DEMO_DOWNLOAD|g" $EBOT_HOME/config/config.ini
+sed -i "s|REMIND_RECORD =.*|REMIND_RECORD = $REMIND_RECORD|g" $EBOT_HOME/config/config.ini
+sed -i "s|DAMAGE_REPORT =.*|DAMAGE_REPORT = $DAMAGE_REPORT|g" $EBOT_HOME/config/config.ini
+sed -i "s|DELAY_READY = .*|DELAY_READY = $DELAY_READY|g" $EBOT_HOME/config/config.ini
+sed -i "s|NODE_STARTUP_METHOD = .*|NODE_STARTUP_METHOD = \"$NODE_STARTUP_METHOD\"|g" $EBOT_HOME/config/config.ini
 
 exec php "$EBOT_HOME/bootstrap.php"
